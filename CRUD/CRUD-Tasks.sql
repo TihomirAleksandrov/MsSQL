469e3fa555a01c7f2b056312105ffff16abf8ac2
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-02-16 14:21:19 +0200

    Added new tasks

--- CRUD/CRUD-Tasks.sql
+++ CRUD/CRUD-Tasks.sql
@@ -19,3 +19,23 @@ FROM Employees AS e
 --Task7
 SELECT DISTINCT e.Salary
 FROM Employees AS e
+
+--Task8
+SELECT *
+FROM Employees AS e
+WHERE e.JobTitle = 'Sales Representative'
+
+--Task9
+SELECT e.FirstName, e.LastName, e.JobTitle 
+FROM Employees AS e
+WHERE e.Salary BETWEEN 20000 AND 30000
+
+--Task10
+SELECT e.FirstName + ' ' + ISNULL(e.MiddleName, '') + ' ' + e.LastName AS 'Full Name'
+FROM Employees AS e
+WHERE e.Salary = 25000 OR e.Salary = 14000 OR e.Salary = 12500 OR e.Salary = 23600
+
+--Task11
+SELECt e.FirstName, e.LastName
+FROM Employees AS e
+WHERE e.ManagerID IS NULL
