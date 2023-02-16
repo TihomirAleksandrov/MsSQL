baa88c7932a59a518b79f42ae8faa70a96ce5616
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-02-16 13:30:59 +0200

    Added new tasks

--- CRUD/CRUD-Tasks.sql
+++ CRUD/CRUD-Tasks.sql
@@ -7,3 +7,15 @@ SELECT d.Name FROM Departments AS d
 --Task4
 SELECT e.FirstName, e.LastName, e.Salary 
 FROM Employees AS e
+
+--Task5
+SELECT e.FirstName, e.MiddleName, e.LastName
+FROM Employees AS e
+
+--Task6
+SELECT e.FirstName + '.' + e.LastName + '@softuni.bg' AS 'Full Email Address'
+FROM Employees AS e
+
+--Task7
+SELECT DISTINCT e.Salary
+FROM Employees AS e
