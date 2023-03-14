81de89e4d853728dc897aa51242af6684dc8caca
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-14 12:11:52 +0200

    Added new queries

---
+++ TableRelations/TableRelations-Exercise.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE[TableRelations]
+
+GO
+
+USE[TableRelations]
+
+GO
+
+--Problem 1
+
+CREATE TABLE[Passports]
+(
+[PassportID] INT PRIMARY KEY IDENTITY(101, 1),
+[PassportNumber] NVARCHAR(8) NOT NULL
+)
+
+CREATE TABLE[Persons]
+(
+[]
+)
