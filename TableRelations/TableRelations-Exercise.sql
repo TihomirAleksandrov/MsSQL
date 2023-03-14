5f9ab1cc9d3680401ca44f89303694a62aa40320
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-14 12:37:04 +0200

    Extended query

--- TableRelations/TableRelations-Exercise.sql
+++ TableRelations/TableRelations-Exercise.sql
@@ -33,3 +33,19 @@ VALUES
 ('Roberto', 43300.00, 102),
 ('Tom', 56100.00, 103),
 ('Yana', 60200.00, 101)
+
+--Task 2
+
+CREATE TABLE[Manufacturers]
+(
+[ManufacturerID] INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(50) NOT NULL,
+[EstablishedOn] DATETIME2 NOT NULL
+)
+
+CREATE TABLE[Models]
+(
+[ModelID] INT PRIMARY KEY IDENTITY(101, 1),
+[Name] NVARCHAR(50) NOT NULL,
+[ManufacturerID] INT FOREIGN KEY REFERENCES [Manufacturers]([ManufacturerID]) NOT NULL
+)
