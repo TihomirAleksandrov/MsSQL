cd7b838641b1823a459a82d061e376879c6999ae
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-14 12:42:04 +0200

    Extended query

--- TableRelations/TableRelations-Exercise.sql
+++ TableRelations/TableRelations-Exercise.sql
@@ -49,3 +49,18 @@ CREATE TABLE[Models]
 [Name] NVARCHAR(50) NOT NULL,
 [ManufacturerID] INT FOREIGN KEY REFERENCES [Manufacturers]([ManufacturerID]) NOT NULL
 )
+
+INSERT INTO[Manufacturers]([Name], [EstablishedOn])
+VALUES
+('BMW', '07/03/1916'),
+('Tesla', '01/01/2003'),
+('Lada', '01/05/1966')
+
+INSERT INTO[Models]([Name], [ManufacturerID])
+VALUES
+('X1', 1),
+('i6', 1),
+('Model S', 2),
+('Model X', 2),
+('Model 3', 2),
+('Nova', 3)
