a71d0b4c977a45827fc37c9d2c0f2f2220df431e
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-14 12:27:40 +0200

    Extended query

--- TableRelations/TableRelations-Exercise.sql
+++ TableRelations/TableRelations-Exercise.sql
@@ -21,3 +21,15 @@ CREATE TABLE[Persons]
 [Salary] DECIMAL(8, 2),
 [PassportID] INT FOREIGN KEY REFERENCES [Passports]([PassportID]) UNIQUE NOT NULL
 )
+
+INSERT INTO[Passports]([PassportNumber])
+VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+INSERT INTO[Persons]([FirstName], [Salary], [PassportID])
+VALUES
+('Roberto', 43300.00, 102),
+('Tom', 56100.00, 103),
+('Yana', 60200.00, 101)
