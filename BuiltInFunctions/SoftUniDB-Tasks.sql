6f4634f24ad7f92db4141006d776237ae933e20a
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-15 12:30:50 +0200

    Extended query

--- BuiltInFunctions/SoftUniDB-Tasks.sql
+++ BuiltInFunctions/SoftUniDB-Tasks.sql
@@ -26,5 +26,21 @@ WHERE LEN(t.Name) IN(5,6)
 ORDER BY t.Name
 
 --Task6
-SELECT t.Name
+SELECT t.TownID, t.Name
 FROM Towns AS t
+WHERE LEFT (t.Name, 1) IN ('M', 'K', 'B', 'E')
+ORDER BY t.Name
+
+--Task7
+SELECT t.TownID, t.Name 
+FROM Towns AS t
+WHERE LEFT (t.Name, 1) NOT IN ('R', 'B', 'D')
+ORDER BY t.Name
+
+--Task8
+CREATE VIEW [V_EmployeesHiredAfter2000] AS
+SELECT [FirstName], [LastName]
+FROM Employees
+WHERE YEAR([HireDate]) > 2000
+
+SELECT * FROM [V_EmployeesHiredAfter2000]
