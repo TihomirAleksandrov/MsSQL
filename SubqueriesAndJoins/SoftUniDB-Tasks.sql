113b5e9d336e2e5801b34cb0406f729f160e437f
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-16 14:02:54 +0200

    Extended query

--- SubqueriesAndJoins/SoftUniDB-Tasks.sql
+++ SubqueriesAndJoins/SoftUniDB-Tasks.sql
@@ -37,3 +37,56 @@ LEFT JOIN EmployeesProjects AS ep
 ON e.EmployeeID = ep.EmployeeID
 WHERE ep.ProjectID IS NULL
 ORDER BY e.EmployeeID
+
+--Task6
+SELECT e.FirstName, e.LastName, e.HireDate, d.Name AS 'DeptName'
+FROM Employees AS e
+JOIN Departments AS d
+ON e.DepartmentID = d.DepartmentID
+WHERE e.HireDate > '1999-01-01'
+AND d.Name IN ('Sales', 'Finance')
+ORDER BY e.HireDate
+
+--Task7
+SELECT TOP(5) e.EmployeeID, E.FirstName, P.Name AS 'ProjectName'
+FROM Employees AS e
+JOIN EmployeesProjects AS ep
+ON e.EmployeeID = ep.EmployeeID
+JOIN Projects AS p
+ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002-08-13'
+AND p.EndDate IS NULL
+
+--Task8
+SELECT e.EmployeeID, e.FirstName,
+CASE
+WHEN YEAR(p.StartDate) >= '2005' THEN NULL
+ELSE p.Name
+END 
+AS 'ProjectName'
+FROM Employees AS e
+JOIN EmployeesProjects AS ep
+ON e.EmployeeID = ep.EmployeeID
+JOIN Projects AS p
+ON ep.ProjectID = p.ProjectID
+WHERE e.EmployeeID = 24
+
+--Task9
+SELECT e.EmployeeID, e.FirstName, e.ManagerID, m.FirstName AS 'ManagerName'
+FROM Employees AS e
+JOIN Employees AS m
+ON e.ManagerID = m.EmployeeID
+WHERE e.ManagerID IN (3, 7)
+ORDER BY e.EmployeeID
+
+--Task10
+SELECT TOP(50) e.EmployeeID, 
+CONCAT(e.FirstName, ' ', e.LastName) AS 'EmployeeName',
+CONCAT(m.FirstName, ' ', m.LastName) AS 'ManagerName',
+d.Name AS 'DepartmentName'
+FROM Employees AS e
+JOIN Employees AS m
+ON e.ManagerID = m.EmployeeID
+JOIN Departments AS d
+ON e.DepartmentID = d.DepartmentID
+ORDER BY e.EmployeeID
