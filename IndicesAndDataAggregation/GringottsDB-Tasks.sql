c7764f49559a57754f47d53ea4dffebffd1907d8
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-17 13:50:05 +0200

    Extended query

--- IndicesAndDataAggregation/GringottsDB-Tasks.sql
+++ IndicesAndDataAggregation/GringottsDB-Tasks.sql
@@ -30,3 +30,47 @@ SUM(w.DepositAmount) AS 'TotalSum'
 FROM WizzardDeposits AS w
 WHERE w.MagicWandCreator = 'Ollivander family'
 GROUP BY w.DepositGroup
+
+--Task7
+SELECT w.DepositGroup,
+SUM(w.DepositAmount) AS 'TotalSum'
+FROM WizzardDeposits AS w
+WHERE w.MagicWandCreator = 'Ollivander family'
+GROUP BY w.DepositGroup
+HAVING SUM(w.DepositAmount) < 150000
+ORDER BY TotalSum DESC
+
+--Task8
+SELECT w.DepositGroup, w.MagicWandCreator,
+MIN(w.DepositCharge) AS 'MinDepositCharge'
+FROM WizzardDeposits AS w
+GROUP BY w.DepositGroup, w.MagicWandCreator
+ORDER BY w.MagicWandCreator, w.DepositGroup
+
+--Task9
+SELECT AgeGroup,
+COUNT(*) AS 'WizardCount'
+FROM
+(
+SELECT
+CASE
+WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+ELSE '[61+]'
+END
+AS [AgeGroup]
+FROM WizzardDeposits
+)
+AS [AgeGroupSubquery]
+GROUP BY AgeGroup
+
+--Task10
+SELECT LEFT(FirstName, 1) AS 'FirstLetter'
+FROM WizzardDeposits
+WHERE DepositGroup = 'Troll Chest'
+GROUP BY SUBSTRING(FirstName, 1, 1)
+ORDER BY FirstLetter
