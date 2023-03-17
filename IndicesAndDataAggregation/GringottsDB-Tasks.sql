dede9aad96c053c30ffd7d6985b4a14aef933a6d
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-17 13:19:22 +0200

    Extended query

--- IndicesAndDataAggregation/GringottsDB-Tasks.sql
+++ IndicesAndDataAggregation/GringottsDB-Tasks.sql
@@ -1,3 +1,32 @@
 --Task1
 SELECT COUNT(*) AS 'Count'
 FROM WizzardDeposits
+
+--Task2
+SELECT MAX(w.MagicWandSize) AS 'LongestMagicWand'
+FROM WizzardDeposits AS w
+
+--Task3
+SELECT w.DepositGroup,
+MAX(w.MagicWandSize) AS 'LongestMagicWand'
+FROM WizzardDeposits AS w
+GROUP BY w.DepositGroup
+
+--Task4
+SELECT TOP(2) w.DepositGroup
+FROM WizzardDeposits AS w
+GROUP BY w.DepositGroup
+ORDER BY AVG(w.MagicWandSize)
+
+--Task5
+SELECT w.DepositGroup,
+SUM(w.DepositAmount) AS 'TotalSum'
+FROM WizzardDeposits AS w
+GROUP BY w.DepositGroup
+
+--Task6
+SELECT w.DepositGroup,
+SUM(w.DepositAmount) AS 'TotalSum'
+FROM WizzardDeposits AS w
+WHERE w.MagicWandCreator = 'Ollivander family'
+GROUP BY w.DepositGroup
